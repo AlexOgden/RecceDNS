ebfc5a643c75894e0be04f6686a9af8499080292
Alex Ogden <1379601+AlexOgden@users.noreply.github.com>
2024-09-26 17:24:42 +0100

    Refactor read_from_file function

--- src/io/wordlist.rs
+++ src/io/wordlist.rs
@@ -1,31 +1,29 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Wordlist File not found: {0}")]
+    #[error("Wordlist file not found: {0}")]
     FileNotFound(String),
-    
+
     #[error("Error reading file: {0}")]
     ReadError(String),
 }
 
-pub fn read_from_file(file_path: &str) -> Result<Vec<String>, Error> {
-    let file = File::open(file_path).map_err(|e| {
-        if e.kind() == io::ErrorKind::NotFound {
-            Error::FileNotFound(file_path.to_string())
-        } else {
-            Error::ReadError(file_path.to_string())
-        }
+pub fn read_from_file<P: AsRef<Path>>(file_path: P) -> Result<Vec<String>, Error> {
+    let file_path_ref = file_path.as_ref();
+    let file = File::open(file_path_ref).map_err(|e| match e.kind() {
+        io::ErrorKind::NotFound => Error::FileNotFound(file_path_ref.display().to_string()),
+        _ => Error::ReadError(file_path_ref.display().to_string()),
     })?;
 
     let reader = BufReader::new(file);
-    let mut lines = Vec::new();
-    for line in reader.lines() {
-        let line = line.map_err(|_| Error::ReadError(file_path.to_string()))?;
-        lines.push(line);
-    }
+    let lines: Result<Vec<_>, _> = reader
+        .lines()
+        .map(|line| line.map_err(|_| Error::ReadError(file_path_ref.display().to_string())))
+        .collect();
 
-    Ok(lines)
+    lines
 }
