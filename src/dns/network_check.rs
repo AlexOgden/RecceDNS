985872631c01ee53ce1503e03e6fa8488f76bcff
Alex Ogden <1379601+AlexOgden@users.noreply.github.com>
2024-10-03 11:14:20 +0100

    refactor(network_check): update all error handling to anyhow

--- src/dns/network_check.rs
+++ src/dns/network_check.rs
@@ -1,38 +1,38 @@
+use anyhow::{anyhow, Context, Result};
 use colored::Colorize;
-
-use crate::dns::resolver::resolve_domain;
-use crate::dns::types::QueryType;
 use std::{
     net::{SocketAddr, UdpSocket},
     time::Duration,
 };
 
+use crate::dns::resolver::resolve_domain;
+use crate::dns::types::QueryType;
+
 const DNS_PORT: u16 = 53;
 const ROOT_SERVER: &str = "a.rootservers.net";
 
-fn check_udp_connection(socket: &UdpSocket, server_ip: &str, port: u16) -> Result<(), String> {
+fn check_udp_connection(socket: &UdpSocket, server_ip: &str, port: u16) -> Result<()> {
     let server_addr: SocketAddr = format!("{server_ip}:{port}")
         .parse()
-        .map_err(|e| format!("Failed to parse address: {e}"))?;
+        .context("Failed to parse address")?;
 
     // Set read and write timeout to 2 seconds
     socket
         .set_read_timeout(Some(Duration::from_secs(2)))
-        .map_err(|e| format!("Failed to set read timeout: {e}"))?;
+        .context("Failed to set read timeout")?;
     socket
         .set_write_timeout(Some(Duration::from_secs(2)))
-        .map_err(|e| format!("Failed to set write timeout: {e}"))?;
+        .context("Failed to set write timeout")?;
 
     socket
         .connect(server_addr)
-        .map_err(|e| format!("Failed to connect to server: {e}"))?;
+        .context("Failed to connect to server")?;
 
     Ok(())
 }
 
-fn check_dns_server(server_address: &str) -> Result<(), String> {
-    let socket =
-        UdpSocket::bind("0.0.0.0:0").map_err(|e| format!("Failed to create socket: {e}"))?;
+fn check_dns_server(server_address: &str) -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:0").context("Failed to create socket")?;
 
     check_udp_connection(&socket, server_address, DNS_PORT)?;
 
@@ -41,7 +41,7 @@ fn check_dns_server(server_address: &str) -> Result<(), String> {
         Ok(())
     } else {
         print_status(server_address, "FAIL", "red");
-        Err(String::from(server_address))
+        Err(anyhow!(server_address.to_string()))
     }
 }
 
@@ -61,7 +61,7 @@ fn print_status(server_address: &str, status: &str, color: &str) {
 }
 
 pub fn check_server_list(server_list: &mut Vec<&str>) -> Result<(), Vec<String>> {
-    println!("Checking DNS Resolvers...");
+    println!("Checking DNS Servers...");
 
     let mut failed_servers = Vec::new();
     let mut i = 0;
@@ -69,8 +69,8 @@ pub fn check_server_list(server_list: &mut Vec<&str>) -> Result<(), Vec<String>>
     while i < server_list.len() {
         match check_dns_server(server_list[i]) {
             Ok(()) => i += 1,
-            Err(dead_server) => {
-                failed_servers.push(dead_server);
+            Err(err) => {
+                failed_servers.push(err.to_string());
                 server_list.remove(i);
             }
         }
@@ -90,7 +90,7 @@ mod test {
     #[test]
     fn udp_connect_check_ok() {
         let socket = UdpSocket::bind("0.0.0.0:0")
-            .map_err(|e| format!("Failed to create socket: {e}"))
+            .context("Failed to create socket")
             .unwrap();
 
         let result = check_udp_connection(&socket, "127.0.0.1", 53);
@@ -101,7 +101,7 @@ mod test {
     #[test]
     fn udp_connect_check_fail() {
         let socket = UdpSocket::bind("0.0.0.0:0")
-            .map_err(|e| format!("Failed to create socket: {e}"))
+            .context("Failed to create socket")
             .unwrap();
 
         let result = check_udp_connection(&socket, "999.0.0.1", 53);
