a2f6aa48fae94b9ac2c9abcb87e495f73797d7e9
Alex Ogden <1379601+AlexOgden@users.noreply.github.com>
2024-11-23 18:34:30 +0000

    refactor: change ResolverSelector to use enum for selection strategy

--- src/dns/resolver_selector.rs
+++ src/dns/resolver_selector.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use rand::seq::IteratorRandom;
+use rand::seq::SliceRandom;
 use rand::thread_rng;
 
 use crate::io::cli::CommandArgs;
@@ -8,104 +8,40 @@ pub trait ResolverSelector {
     fn select<'a>(&mut self, dns_resolvers: &'a [&str]) -> Result<&'a str>;
 }
 
-// Selects a DNS resolver randomly from the list of DNS resolvers
-pub struct Random;
-
-impl ResolverSelector for Random {
-    fn select<'a>(&mut self, dns_resolvers: &'a [&str]) -> Result<&'a str> {
-        let mut random_generator = thread_rng();
-        dns_resolvers
-            .iter()
-            .choose(&mut random_generator)
-            .copied()
-            .ok_or_else(|| anyhow::anyhow!("DNS Resolvers list is empty"))
-    }
+pub enum Selector {
+    Random,
+    Sequential { current_index: usize },
 }
 
-// Selects a DNS resolver sequentially from the list of DNS resolvers
-pub struct Sequential {
-    current_index: usize,
-}
-
-impl Sequential {
-    pub const fn new() -> Self {
-        Self { current_index: 0 }
-    }
-}
-
-impl ResolverSelector for Sequential {
-    fn select<'a>(&mut self, dns_resolvers: &'a [&str]) -> Result<&'a str> {
-        if dns_resolvers.is_empty() {
-            return Err(anyhow::anyhow!("DNS Resolvers list is empty"));
+impl Selector {
+    pub const fn new(use_random: bool) -> Self {
+        if use_random {
+            Self::Random
+        } else {
+            Self::Sequential { current_index: 0 }
         }
-        let resolver = dns_resolvers[self.current_index];
-        self.current_index = (self.current_index + 1) % dns_resolvers.len();
-        Ok(resolver)
+    }
+}
+
+impl ResolverSelector for Selector {
+    fn select<'a>(&mut self, dns_resolvers: &'a [&str]) -> Result<&'a str> {
+        match self {
+            Self::Random => dns_resolvers
+                .choose(&mut thread_rng())
+                .copied()
+                .ok_or_else(|| anyhow::anyhow!("DNS Resolvers list is empty")),
+            Self::Sequential { current_index } => {
+                if dns_resolvers.is_empty() {
+                    return Err(anyhow::anyhow!("DNS Resolvers list is empty"));
+                }
+                let resolver = dns_resolvers[*current_index];
+                *current_index = (*current_index + 1) % dns_resolvers.len();
+                Ok(resolver)
+            }
+        }
     }
 }
 
 pub fn get_selector(args: &CommandArgs) -> Box<dyn ResolverSelector> {
-    if args.use_random {
-        Box::new(Random)
-    } else {
-        Box::new(Sequential::new())
-    }
-}
-
-// Unit Tests
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    // Test Random Resolver Selector
-    #[test]
-    fn test_random_resolver_selector() {
-        // Test 5 times
-        for _ in 0..5 {
-            let mut random = Random;
-            let dns_resolvers = ["1.0.0.1", "1.1.1.1", "8.8.8.8"];
-            let resolver = random.select(&dns_resolvers).unwrap();
-            assert!(dns_resolvers.contains(&resolver));
-        }
-    }
-
-    // Test Sequential Resolver Selector
-    #[test]
-    fn test_sequential_resolver_selector() {
-        let mut sequential = Sequential::new();
-        let dns_resolvers = ["1.1.1.1", "1.0.0.1", "8.8.8.8"];
-        for resolver in &dns_resolvers {
-            assert_eq!(sequential.select(&dns_resolvers).unwrap(), *resolver);
-        }
-    }
-
-    // Test Random Resolver Selector with empty list
-    #[test]
-    fn test_random_resolver_selector_empty() {
-        let mut random = Random;
-        let dns_resolvers: [&str; 0] = [];
-        let result = random.select(&dns_resolvers);
-        assert!(result.is_err());
-    }
-
-    // Test Sequential Resolver Selector with empty list
-    #[test]
-    fn test_sequential_resolver_selector_empty() {
-        let mut sequential = Sequential::new();
-        let dns_resolvers: [&str; 0] = [];
-        let result = sequential.select(&dns_resolvers);
-        assert!(result.is_err());
-    }
-
-    // Test Sequential Resolver Selector wrapping around
-    #[test]
-    fn test_sequential_resolver_selector_wrap_around() {
-        let mut sequential = Sequential::new();
-        let dns_resolvers = ["1.1.1.1", "1.0.0.1", "8.8.8.8"];
-        for _ in 0..dns_resolvers.len() * 2 {
-            for resolver in &dns_resolvers {
-                assert_eq!(sequential.select(&dns_resolvers).unwrap(), *resolver);
-            }
-        }
-    }
+    Box::new(Selector::new(args.use_random))
 }
